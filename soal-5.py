99ca6939d5fe9b5a241d6e2d8c1d620d7e5b2d8c
tijekhaled <116840456+tijekhaled@users.noreply.github.com>
2022-10-29 13:23:23 +0700

    Add files via upload

---
+++ soal-5.py
@@ -0,0 +1,18 @@
+# Latihan Soal UTS KU1102 K16
+# Asisten Akademik: Theodore Maximillan Jonathan (13219021)
+
+# Nomor Soal    :
+# NIM           : 
+# Nama          :
+
+# PROGRAM _______________ [isi dengan judul]
+# Tuliskan judul program dan penjelasan singkat.
+
+# KAMUS
+# Isilah dengan definisi variabel yang digunakan.
+
+# ALGORITMA
+# Jangan lupa memberikan komentar secukupnya.
+
+# Seluruh nilai yang lulus disimpan dalam array bernama NilaiLulus. Urutan harus sama dengan urutan input.
+print(NilaiLulus)
